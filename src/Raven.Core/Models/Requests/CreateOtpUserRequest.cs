9fc5f6551571af4879b988ae866c569e7cd80033
olumuyiwa-agboola <agboolaod@gmail.com>
2025-06-02 20:01:43 +0100

    Add more tests

--- src/Raven.Core/Models/Requests/CreateOtpUserRequest.cs
+++ src/Raven.Core/Models/Requests/CreateOtpUserRequest.cs
@@ -9,27 +9,27 @@ namespace Raven.Core.Models.Requests
     /// </summary>
     /// <remarks>This class is used to encapsulate the data required to create a new user obtained 
     /// from the body of a POST request to the relevant endpoint.</remarks>
-    public record CreateOtpUserRequest(string Email, string LastName, string FirstName, string PhoneNumber)
+    public record CreateOtpUserRequest
     {
         /// <summary>
         /// Gets or initializes the email address associated with the user.
         /// </summary>
-        public string Email { get; init; } = Email;
+        public string Email { get; init; } = string.Empty;
 
         /// <summary>
         /// Gets or initializes the last name of the user.
         /// </summary>
-        public string LastName { get; init; } = LastName;
+        public string LastName { get; init; } = string.Empty;
 
         /// <summary>
         /// Gets or initializes the first name of the user.
         /// </summary>
-        public string FirstName { get; init; } = FirstName;
+        public string FirstName { get; init; } = string.Empty;
 
         /// <summary>
         /// Gets or initializes the phone number of the user.
         /// </summary>
-        public string PhoneNumber { get; init; } = PhoneNumber;
+        public string PhoneNumber { get; init; } = string.Empty;
     }
 
     /// <summary>
---
+++ tests/Raven.Tests/UnitTests/RepositoryTests/UsersMySQLRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using FakeItEasy;
+using Raven.Core.Models.Entities;
+using Raven.Core.Abstractions.Factories;
+using Raven.Infrastructure.Repositories.MySQL;
+
+namespace Raven.Tests.UnitTests.RepositoryTests
+{
+    public class UsersMySQLRepositoryTests
+    {
+        private readonly OtpUser _sampleOtpUser;
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+        private readonly UsersMySQLRepository _usersMySQLRepository;
+
+        public UsersMySQLRepositoryTests()
+        {
+            _sampleOtpUser = new Faker<OtpUser>(locale: "en_NG")
+                .RuleFor(x => x.LastName, x => x.Person.LastName)
+                .RuleFor(x => x.FirstName, x => x.Person.FirstName)
+                .RuleFor(x => x.CreatedAt, _ => DateTimeOffset.Now)
+                .RuleFor(x => x.LastUpdatedAt, _ => DateTimeOffset.Now)
+                .RuleFor(x => x.UserId, _ => Ulid.NewUlid().ToString())
+                .RuleFor(x => x.PhoneNumber, x => x.Phone.PhoneNumber())
+                .RuleFor(x => x.EmailAddress, x => x.Internet.Email(x.Person.FirstName, x.Person.LastName));
+
+            _dbConnectionFactory = A.Fake<IDbConnectionFactory>();
+            _usersMySQLRepository = new UsersMySQLRepository(_dbConnectionFactory);
+        }
+
+        [Fact]
+        public async Task SaveOtpUser_TakesOtpUserObject_ReturnsTupleOfTrueAndNull()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
